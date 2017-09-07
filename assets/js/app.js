a0b88e61881be1ea180bd96ea80e8a2db571b657
ponteviti <ponteviti@gmail.com>
2017-09-07 17:53:48 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -308,8 +308,9 @@ $.getJSON("data/DOITT_MUSEUM_01_13SEPT2010.geojson", function (data) {
 
 map = L.map("map", {
   zoom: 10,
-  center: [40.702222, -73.979378],
-  layers: [cartoLight, boroughs, markerClusters, highlight],
+  center: [42.4304, -8.6204],
+  layers: [cartoLight, museums, markerClusters, highlight],
+ /* layers: [cartoLight, boroughs, markerClusters, highlight],*/
   zoomControl: false,
   attributionControl: false
 });
